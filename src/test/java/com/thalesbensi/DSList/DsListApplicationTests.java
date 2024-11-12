e6903cca2276e8eb8137b324943d499fb55c6b2a
Thales Bensi <thalesbensi@outlook.com>
2024-11-12 16:00:22 -0300

    Project Created

---
+++ src/test/java/com/thalesbensi/DSList/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thalesbensi.DSList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
